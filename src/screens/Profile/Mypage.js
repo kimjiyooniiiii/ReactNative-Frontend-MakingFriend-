a4b1a6b93f39fcd8321465117610669d07d190d0
delay-100 <qor3127@naver.com>
2023-07-25 16:29:43 +0900

    FIX: mypage 안드로이드 border 에러 해결

--- src/screens/Profile/Mypage.js
+++ src/screens/Profile/Mypage.js
@@ -17,7 +17,11 @@ const ProfileSectionContainer = styled.View`
   justify-content: center;
   align-items: center;
   background-color: ${({ theme }) => theme.backgroundSkyblue};
-  border-radius: 0% 0% 20% 20%;
+  ${Platform.select({
+    ios: "border-bottom-left-radius: 10 10; border-bottom-right-radius: 10 10;", // iOS용 스타일
+    android:
+      "border-bottom-left-radius: 10px; border-bottom-right-radius: 10px", // Android용 스타일
+  })}
 `;
 
 const DEFAULT_PHOTO =
